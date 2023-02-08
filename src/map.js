d12c77fd3ce3875bf0cd45aaacc52fe7bb3a1ba8
Chiew Yi Xiang <yixiangchiew@gmail.com>
2023-02-09 00:45:40 +0800

    added geocode api

--- src/map.js
+++ src/map.js
@@ -1,8 +1,39 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./marker";
+import Geocode from "react-geocode";
+import { useState, useEffect } from "react";
 
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+/*
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: latlng,
+  };
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+function codeAddress() {
+  var address = document.getElementById("address").value;
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == "OK") {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+*/
 
 export default function SimpleMap() {
   const defaultProps = {
@@ -13,14 +44,33 @@ export default function SimpleMap() {
     zoom: 12,
   };
 
-  const apiURL = process.env.GOOGLE_MAPS_API;
-  console.log(apiURL)
+  const [results, setResults] = useState([]);
+
+  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_GEOCODE_API);
+
+  async function getResult() {
+    
+  }
+
+  useEffect(() => {
+    getResult();
+  }, []);
+
+  Geocode.fromAddress("49 Hume Ave Singapore").then(
+    (response) => {
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log(lat, lng);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
 
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API}}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
         yesIWantToUseGoogleMapApiInternals
